e8bd398a2f97c646de263d7045fea9dcab5142b7
Filipe Silva <filipematossilva@gmail.com>
2017-02-28 11:07:57 +0000

    add basic aot build

---
+++ index.ts
@@ -0,0 +1,4 @@
+// #docregion
+export { HeroProfileComponent } from './src/hero-profile.component';
+export { HeroProfileModule }    from './src/hero-profile.module';
+export { Hero }                 from './src/hero';
