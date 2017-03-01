446e3c3d64d1d20a7c42d8e3816a5ccea7e843f5
Filipe Silva <filipematossilva@gmail.com>
2017-03-01 15:21:24 +0000

    use rollup-plugin-angular-inline

--- rollup-config.js
+++ rollup-config.js
@@ -1,9 +1,14 @@
+import angularInline from 'rollup-plugin-angular-inline';
+
 export default {
   entry: './src/index.js',
   dest: './bundles/quickstart-lib.umd.js',
   format: 'umd',
-  moduleName: 'ng.heroProfile',
+  moduleName: 'ng.quickstartLib',
   globals: {
     '@angular/core': 'ng.core'
-  }
+  },
+  plugins: [
+    angularInline({ include: './src/**/*.component.js' })
+  ]
 }
