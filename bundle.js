bf2efb69446f3deace9b4ee43f7bc80f447c9e97
Filipe Silva <filipematossilva@gmail.com>
2017-03-15 15:46:49 +0000

    add fesm2015 bundle

--- bundle.js
+++ bundle.js
@@ -1,50 +1,75 @@
-const rollup = require('rollup')
-const uglify = require('rollup-plugin-uglify')
+const ngc = require('@angular/compiler-cli/src/main').main;
+const rollup = require('rollup');
+const uglify = require('rollup-plugin-uglify');
 
-// Base configuration.
-const umdDir = `./bundles`;
-const fesmDir = `./dist`;
-const libFilename = 'quickstart-lib';
-const rollupBaseConfig = {
-  entry: './out-tsc/lib/index.js',
-  moduleName: 'quickstartLib',
-  globals: {
-    '@angular/core': 'ng.core'
-  },
-  plugins: []
-};
+const inlineResources = require('./inline-resources');
 
-// UMD bundle.
-const umdConfig = Object.assign({}, rollupBaseConfig, {
-  dest: `${umdDir}/${libFilename}.umd.js`,
-  format: 'umd',
-});
 
-// Minified UMD bundle.
-const minifiedUmdConfig = Object.assign({}, rollupBaseConfig, {
-  dest: `${umdDir}/${libFilename}.umd.min.js`,
-  format: 'umd',
-  plugins: rollupBaseConfig.plugins.concat([uglify({})])
-});
+return Promise.resolve()
+  // Compile to ES5.
+  .then(() => ngc({ project: 'tsconfig.lib.json' }))
+  .then(() => inlineResources('tsconfig.lib.json'))
+  .then(() => console.log('ES5 compilation succeeded.'))
+  // Compile to ES2015.
+  .then(() => ngc({ project: 'tsconfig.lib-es2015.json' }))
+  .then(() => inlineResources('tsconfig.lib-es2015.json'))
+  .then(() => console.log('ES2015 compilation succeeded.'))
+  // Bundle lib.
+  .then(() => {
+    // Base configuration.
+    const umdDir = `./bundles`;
+    const fesmDir = `./dist`;
+    const libFilename = 'quickstart-lib';
+    const rollupBaseConfig = {
+      entry: './out-tsc/lib/index.js',
+      moduleName: 'quickstartLib',
+      globals: {
+        '@angular/core': 'ng.core'
+      },
+      external: [
+        '@angular/core'
+      ],
+      plugins: []
+    };
 
-// ESM+ES5 flat module bundle.
-const fesm2015config = Object.assign({}, rollupBaseConfig, {
-  dest: `${fesmDir}/${libFilename}.js`,
-  format: 'es'
-});
+    // UMD bundle.
+    const umdConfig = Object.assign({}, rollupBaseConfig, {
+      dest: `${umdDir}/${libFilename}.umd.js`,
+      format: 'umd',
+    });
 
-// ESM+ES2015 flat module bundle.
+    // Minified UMD bundle.
+    const minifiedUmdConfig = Object.assign({}, rollupBaseConfig, {
+      dest: `${umdDir}/${libFilename}.umd.min.js`,
+      format: 'umd',
+      plugins: rollupBaseConfig.plugins.concat([uglify({})])
+    });
 
-const allBundles = [
-  umdConfig,
-  minifiedUmdConfig,
-  fesm2015config
-].map(cfg => rollup.rollup(cfg).then(bundle => bundle.write(cfg)));
+    // ESM+ES5 flat module bundle.
+    const fesm5config = Object.assign({}, rollupBaseConfig, {
+      dest: `${fesmDir}/${libFilename}.es5.js`,
+      format: 'es'
+    });
 
-return Promise.all(allBundles)
-  .then(() => console.log('\nAll bundles generated successfully.'))
-  .catch(e => {
-    console.error('\nBundle generation failed. See below for errors.\n');
-    console.error(e);
-    process.exit(1);
+    // ESM+ES2015 flat module bundle.
+    const fesm2015config = Object.assign({}, rollupBaseConfig, {
+      entry: './out-tsc/lib-es2015/index.js',
+      dest: `${fesmDir}/${libFilename}.js`,
+      format: 'es'
+    });
+
+    const allBundles = [
+      umdConfig,
+      minifiedUmdConfig,
+      fesm5config,
+      fesm2015config
+    ].map(cfg => rollup.rollup(cfg).then(bundle => bundle.write(cfg)));
+
+    return Promise.all(allBundles)
+      .then(() => console.log('All bundles generated successfully.'))
+      .catch(e => {
+        console.error('\nBundle generation failed. See below for errors.\n');
+        console.error(e);
+        process.exit(1);
+      });
   });
