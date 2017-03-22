839516a4217b23cf930eaf7eece967b682ecd64f
Filipe Silva <filipematossilva@gmail.com>
2017-03-22 18:04:06 +0000

    add externals/globals comments

--- build.js
+++ build.js
@@ -50,10 +50,18 @@ return Promise.resolve()
       entry: `./out-tsc/lib/${libName}.js`,
       moduleName: camelCase(libName),
       sourceMap: true,
+      // ATTENTION:
+      // Add any dependency or peer dependency your library to `globals` and `external`.
+      // This is required for UMD bundle users.
       globals: {
+        // The key here is library name, and the value is the the name of the global variable name
+        // the window object.
+        // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals for more.
         '@angular/core': 'ng.core'
       },
       external: [
+        // List of dependencies
+        // See https://github.com/rollup/rollup/wiki/JavaScript-API#external for more.
         '@angular/core'
       ],
       plugins: [
