70f6498c806ba10439e79e1ee313eb1a2e2cf336
Filipe Silva <filipematossilva@gmail.com>
2017-03-22 12:27:33 +0000

    don't provide LibService in demo

--- src/demo/app/app.module.ts
+++ src/demo/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { LibModule, LibService } from 'angular-quickstart-lib';
+import { LibModule } from 'angular-quickstart-lib';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule, LibModule],
   declarations: [ AppComponent ],
-  providers:    [ LibService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
